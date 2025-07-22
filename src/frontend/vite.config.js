ffc31bc59168db63865362c013fd38c965324b97
pixeliy <pixeliy.app@gmail.com>
2025-07-22 16:47:00 +0800

    feat: setup shadcn/ui components

--- src/frontend/vite.config.js
+++ src/frontend/vite.config.js
@@ -35,6 +35,12 @@ export default defineConfig({
   resolve: {
     alias: [
       {
+        find: "@",
+        replacement: fileURLToPath(
+          new URL("./src", import.meta.url)
+        ),
+      },
+      {
         find: "declarations",
         replacement: fileURLToPath(
           new URL("../declarations", import.meta.url)
